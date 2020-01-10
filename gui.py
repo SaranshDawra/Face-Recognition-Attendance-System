0c738c6f3e41a441803e699f9373dcc402f770c6
SaranshDawra <saransh2000dawra@gmail.com>
2020-01-10 21:28:41 +0530

    Create Data GUI

--- gui.py
+++ gui.py
@@ -7,13 +7,38 @@ Created on Sat Jan  4 21:07:05 2020
 
 import tkinter as tk
 
+def createData():
+    root = tk.Tk()
+    root.title("Student Information")
+    
+    name = tk.StringVar()
+    
+    enterName = tk.Label(root,text="Enter Name",width=15,font=("times new roman",20))
+    enterName.grid(row=0,column=0,padx=10,pady=10)
+    
+    userName = tk.Entry(root,width = 25,textvariable = name,font=("times new roman",20),justify='center')
+    userName.grid(row=0,column=1,padx=10,pady=10)
+    
+    ID = tk.StringVar()
+    
+    enterId = tk.Label(root,text="Enter ID",width=15,font=("times new roman",20))
+    enterId.grid(row=1,column=0,padx=10,pady=10)
+    
+    userId = tk.Entry(root,width = 25,textvariable = ID,font=("times new roman",20),justify='center')
+    userId.grid(row=1,column=1,padx=10,pady=10)
+    
+    submit = tk.Button(root,text="Submit",width=15,font=("times new roman",20),bg="#000000",fg='white',anchor='center')
+    submit.grid(row=2,column=0,padx=(500,10),pady=20)
+    
+    root.mainloop()
+
 def exitFunction():
     window.destroy()
 
 window = tk.Tk()
 window.title("Face Recognition Attendance System")
 
-dataset_button = tk.Button(window,text="Create Dataset",width=25,font=("times new roman",20),bg="#000000",fg='white')
+dataset_button = tk.Button(window,text="Create Dataset",width=25,font=("times new roman",20),bg="#000000",fg='white',command=createData)
 dataset_button.pack(padx = 10 , pady = 10)
 
 update_button = tk.Button(window, text="Train", width=25,font=("times new roman",20),bg="#000000",fg='white')
